620e83b0418450f0fcc5d19e838307efd519f2c3
Rishu Kumar <126085066+rishu-7549@users.noreply.github.com>
2025-02-04 23:41:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 var isDate = function (input) {
   //   write your code here
+	return !isNan(Date.parse(input));
 };
 
 // Do not change the code below.
 const input = prompt("Enter Date.");
 alert(isDate(input));
+
